b7098eaae1f0d19cbd078e6d367dc18a853b94d2
Saif <saifisami2005@gmail.com>
2024-12-02 20:09:58 -0500

    Created app.js.

---
+++ part-2/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+    return(
+        <div>
+            <TweetComponent name="John Doe" username="john_doe" date="01/01/2008" message="This is my tweet."/>
+            <TweetComponent name="Jane Doe" username="jane_doe" date="05/05/2009" message="This is an example also."/>
+            <TweetComponent name="Penelope Doe" username="penelope_doe" date="08/03/2010" message="Very good application."/>
+            <TweetComponent name="George Doe" username="george_doe" date="12/12/2011" message="I don't like this."/>
+            <TweetComponent name="Matthew Doe" username="matthew_doe" date="08/11/2014" message="Whatever this is."/>
+        </div>
+    )
+}
